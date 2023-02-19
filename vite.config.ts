4b303163236f416d17bb015d7993aeaa8956616a
Alvolutionor <haoran.jin@uqconnect.edu.au>
2023-02-19 10:03:55 +1000

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+<<<<<<< Updated upstream
     basicSsl(),
+=======
+>>>>>>> Stashed changes
   ],
   
 });
