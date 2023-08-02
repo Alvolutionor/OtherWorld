3f07979a381ab86488ff2dab03a66fb9808f9215
KimJ <108210462+Alvolutionor@users.noreply.github.com>
2023-08-02 14:24:45 +0900

    Revert "as"
    This reverts commit fd036f39d89787bd418952f361a228d0f490a5f4.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+<<<<<<< Updated upstream
+    basicSsl(),
+=======
+>>>>>>> Stashed changes
   ],
   
 });
